55868cb75d226a367323c7741d8058df416f6152
Chirag Mittal <118048068+chiragmittal36269@users.noreply.github.com>
2023-12-26 16:44:05 +0000

    install react

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDom from "react-dom/client";
+import App from "./App";
+
+// Create a root for concurrent rendering
+const root = ReactDom.createRoot(document.getElementById("root"));
+
+// Render the main App component into the root
+root.render(<App />);
