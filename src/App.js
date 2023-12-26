20d70a4bfa3242f6084a9e75cbf5424eca83f688
Chirag Mittal <chiragmittal36269@gmail.com>
2023-12-26 23:06:13 +0530

    add partial css

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import CreatePoint from "./Components/CreatePoint";
 import View from "./Components/View";
+import "./style.css";
 
 function App() {
     return (
